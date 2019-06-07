52bfd2c98489965e816db1a17d23d631cc5da0da
EugeneLazakovich <zlazakovich@gmail.com>
2019-06-07 14:32:40 +0300

    fix 6.1

--- 6.1.cs
+++ 6.1.cs
@@ -6,18 +6,23 @@ namespace _6._1
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            int index1 = name.IndexOf(" ");
-            string surname = name.Substring(0, index1);
-            string name1 = name.Substring(index1 + 1);
-            int index2 = name1.IndexOf(" ");
-            string firstName = name1.Substring(0, index2);
-            firstName = firstName.Substring(0, 1).ToUpper() + '.';
-            string secondName = name1.Substring(index2 + 1);
-            secondName = secondName.Substring(0, 1).ToUpper() + '.';
+            string name = Console.ReadLine();            
+            string[] words = name.Split(' ');
+            string surname = "";
+            string firstName = "";
+            string secondName = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                surname = words[0];
+                firstName = words[1];
+                secondName = words[2];
+            }
             string[] arr = new string[surname.Length];
             arr[0] = surname[0].ToString().ToUpper();
             surname = arr[0] + surname.Substring(1, surname.Length - 1);
+            firstName = firstName.Substring(0, 1).ToUpper() + '.';
+            secondName = secondName.Substring(0, 1).ToUpper() + '.';
+            Console.Clear();
             Console.Write(surname + ' ');
             Console.Write(firstName);
             Console.WriteLine(secondName);
